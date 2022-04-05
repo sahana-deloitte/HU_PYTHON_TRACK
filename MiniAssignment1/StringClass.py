3e3499f397cb3f6ef796ab31376eb367ba8d1d34
K H <sahankh@deloitte.com>
2022-04-05 14:19:48 +0530

    First Assignment

--- MiniAssignment1/StringClass.py
+++ MiniAssignment1/StringClass.py
@@ -1,43 +1,43 @@
 class StringClass:
-    def __init__(self, demo):
-        self.demo = demo
+    def __init__(self, name):
+        self.name = name
 
-    def lis(self):
-        return (list(self.demo))
+    def list(self):
+        return (list(self.name))
 
     def length(self):
-        return len(self.demo)
+        return len(self.name)
 
 
 class PairsPossible(StringClass):
     def pair(self, test_list):
-        res = [(n, object1) for idx, n in enumerate(test_list) for object1 in test_list[idx + 1:]]
+        res = [(num, object1) for idx, num in enumerate(test_list) for object1 in test_list[idx + 1:]]
         print(res)
 
 
 class SearchCommonElements(StringClass):
-    def common(self, st, demo):
-        a = list(set(st) & set(demo))
+    def common(self, str, name):
+        a = list(set(str) & set(name))
         print(a)
 
 
 class EqualSumPairs:
     def pair2(self, test_list):
-        res = [sum((int(n), int(object1))) for idx, n in enumerate(test_list) for object1 in test_list[idx + 1:]]
+        res = [sum((int(num), int(object1))) for idx, num in enumerate(test_list) for object1 in test_list[idx + 1:]]
         print(set(res))
         print(len(set(res)))
 
 
-n = input("Enter a string :")
-object1 = StringClass(n)
-print(object1.lis())
+num = input("Enter a string :")
+object1 = StringClass(num)
+print(object1.list())
 print(object1.length())
-h = object1.lis()
+h = object1.list()
 object2 = PairsPossible(object1)
 object2.pair(h)
 st = input("Enter string to compare ")
 object3 = SearchCommonElements(object1)
-object3.common(n, st)
+object3.common(num, str)
 object4 = EqualSumPairs()
 object4.pair2(h)
 
